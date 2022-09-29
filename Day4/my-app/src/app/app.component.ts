cf7c62880440c4efee4c4544b3291fbbb7c68003
Quentin Gonus <quentingonus@gmail.com>
2022-09-29 11:08:10 +0630

    Day 4 Add

--- Day4/my-app/src/app/app.component.ts
+++ Day4/my-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
 import { DebugService } from './services/debug.service';
 
 @Component({
@@ -6,9 +6,9 @@ import { DebugService } from './services/debug.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title: string = 'Expense Manager';
   constructor(private debugService : DebugService) {} ngOnInit() { 
-    this.debugService.info("Angular Application starts"); 
+    this.debugService.info("Angular Application starts");
  }
 }
---
+++ Day4/my-app/src/app/services/debug.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DebugService {
+
+  constructor() { }
+  info(message: string): void{
+    console.log(message)
+  }
+}
